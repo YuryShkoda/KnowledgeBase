cf83ac8cd973ec8ebab2f11444b76469cfc84926
Yury Shkoda <shkoda.yuri@gmail.com>
2018-01-09 20:29:14 -0500

    Tips and Tricks for Xcode

---
+++ XcodeTipsAndTricks.playground/Contents.swift
@@ -0,0 +1,19 @@
+/*
+                                        Tips and Tricks
+    Cmd+Shift+O ~ open quickly
+    Cmd+Shift+J ~ show file in file novigator
+    Alt+Shift+Click on file ~ how to open a file
+    Ctrl+Cmd+J ~ jump to defenition
+    // MARK: -
+    ALt+Cmd+/ - add description
+    Alt+Shift+Cmd+K - run clean project
+    Cmd+\ ~ add breackpoint
+    Cmd+Y ~ on/off breackpoints
+    Ctrl+Alt+Cmd+3 ~ object library filter
+    Alt+Cmd+3 ~ indentity inspector
+    Shift+Alt+Cmd+= ~ reset to suggested constraints
+    Alt+Cmd+Return ~ show assistant editor
+    Fn+F6 ~ step over
+    Ctrl+Cmnd+Y ~ continue execution
+    Cmnd+L ~ lock simulator
+ */
